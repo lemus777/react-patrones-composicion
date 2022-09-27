10806a9c416959a57987893d5e0e01064bc39061
lemus <lemus777@gmail.com>
2022-09-27 12:36:25 +0200

    estructura simulada para prueba

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,59 @@
 import React from 'react';
-import { TodoProvider } from '../TodoContext/index';
-import { AppUI } from './AppUI';
-
+//import { TodoProvider } from '../TodoContext/index';
+//import { TodoSearch } from '../TodoSearch';
+//import { AppUI } from './AppUI';
 
 function App() {
+  const [state, setState] = React.useState('estado compartido');
+
   return (
-    <TodoProvider>
-      <AppUI />
-    </TodoProvider>
+    <React.Fragment>
+      <TodoHeader>
+        <TodoCounter />
+        <TodoSearch />
+      </TodoHeader>
+
+      <TodoList>
+        <TodoItem state={state} />
+      </TodoList>
+    </React.Fragment>
   );
 }
+
+function TodoHeader({ children }) {
+  return (
+    <header>
+      {children}
+    </header>
+  );
+}
+
+function TodoList({ children }) {
+  return (
+    <section className='TodoList-container'>
+      {children}
+    </section>
+  );
+}
+
+function TodoCounter() {
+  return <p>TodoCounter</p>;
+}
+
+function TodoSearch() {
+  return <p>TodoSearch</p>;
+}
+
+function TodoItem({state}) {
+  return <p>TodoItem: {state}</p>;
+}
+
+//function App() {
+//  return (
+//    <TodoProvider>
+//      <AppUI />
+//    </TodoProvider>
+//  );
+//}
 
 export default App;
