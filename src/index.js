0adbbeb3d3965d9394010b76462a5f03303385c2
lemus <lemus777@gmail.com>
2022-10-01 15:07:09 +0200

    la locura de las HOC

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App/index.js';
+//import App from './App/index.js';
+
+function App(props) {
+    return(
+        <h1>¡{props.saludo}, {props.nombre}!</h1>
+    );
+}
+
+function withSaludo(WrappedComponent) {
+    return function WrappedComponentWithSaludo (saludo) {
+        return function componenteDeVerdad(props) {
+            return (
+                <React.Fragment>
+                    <WrappedComponent {...props} saludo={saludo} />
+                    <p>Estamos acompañando al WrappedComponent</p>
+                </React.Fragment>
+            );
+        }
+    }
+}
+
+const AppWithSaludo = withSaludo(App)('Wenas');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />,
+    <AppWithSaludo nombre='Carolina' />
+    //<App saludo='Buenas' nombre='Nath' />,
 );
 
 
